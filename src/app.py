d7b008781abe8c287ade6d62ce3bcc4ad098bd73
ramil <ramil1-9-9-9@yandex.ru>
2022-11-08 15:47:49 +0300

    feat(app): add database executors

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, status
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, status, Body
+from fastapi.responses import JSONResponse
 import psycopg2
-
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,23 +10,29 @@ conn = psycopg2.connect(dbname='persons', user='program',
                         password='test', host='localhost')
 
 
-class Person:
-    def __init__(self, id, name, age=None, address=None, work=None):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.address = address
-        self.work = work
+class PersonRes(BaseModel):
+    id: int
+    name: str
+    age: Optional[int] = None
+    address: Optional[str] = None
+    work: Optional[str] = None
 
 
-def get_person_by_id(id):
+class PersonReq(BaseModel):
+    name: str
+    age: Optional[int] = None
+    address: Optional[str] = None
+    work: Optional[str] = None
+
+
+def get_person_by_id(person_id):
     with conn.cursor() as cur:
         cur.execute(
-            f"SELECT Id, Name, Age, Work, Address FROM persons WHERE id = {id}"
+            f"SELECT Id, Name, Age, Work, Address FROM persons WHERE Id = {person_id}"
         )
         person_data = cur.fetchone()
 
-        return Person(*person_data) if person_data else None
+        return PersonRes(*person_data) if person_data else None
 
 
 def get_persons_all():
@@ -35,7 +42,40 @@ def get_persons_all():
         )
         person_data = cur.fetchall()
 
-        return [Person(*persons_list[i]) for i in len(person_data)] if person_data else None
+        return [PersonRes(*persons_list[i]) for i in len(person_data)] if person_data else None
+
+
+def person_insert(person: PersonReq):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"INSERT INTO persons(Name, Age, Work, Address) \
+              VALUES ({person.name}, {person.age}, {person.work}, {person.address}) \
+              SELECT currval(pg_get_serial_sequence('persons','Id'))"
+        )
+        conn.commit()
+        row = cur.fetchone()
+        return row
+
+
+def person_delete(person_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"DELETE FROM persons WHERE Id = {person_id}"
+        )
+        conn.commit()
+
+
+def person_patch(person_id, person: PersonReq):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"UPDATE person SET Name = {person.name}" +
+            f", Age = {person.age}" if person.age else "" +
+            f", Address = {person.address}" if person.address else "" +
+            f", Work = {person.work}" if person.work else "" +
+            f"WHERE Id = {person_id}"
+        )
+        conn.commit()
+    return get_person_by_id(person_id)
 
 
 @app.get("/persons/{person_id}")
@@ -55,15 +95,16 @@ def get_all_persons():
 
 
 @app.post("/persons")
-def post_person():
-    pass
+def post_person(person: PersonReq):
+    person_insert(person)
 
 
 @app.patch("/persons/{person_id}")
-def patch_person(person_id):
+def patch_person(person_id, person: PersonReq):
     pass
 
 
 @app.delete("/persons/{person_id}")
-def delete_person():
-    pass
+def delete_person(person_id: int):
+    person_delete(person_id)
+    return JSONResponse(content="", status_code=status.HTTP_204_NO_CONTENT)
