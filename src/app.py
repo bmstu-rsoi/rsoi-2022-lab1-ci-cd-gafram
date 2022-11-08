c87f7a14a69c5c562771e082f90cb8b981e947ab
ramil <ramil1-9-9-9@yandex.ru>
2022-11-08 18:34:54 +0300

    feat(app): add all server functions(local test's passed)

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, status, Body
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 import psycopg2
-from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
 from typing import Optional
+import json
+from pydantic.json import pydantic_encoder
 
 app = FastAPI()
 
@@ -10,57 +12,58 @@ conn = psycopg2.connect(dbname='persons', user='program',
                         password='test', host='localhost')
 
 
-class PersonRes(BaseModel):
+@dataclass
+class PersonRes:
     id: int
     name: str
     age: Optional[int] = None
-    address: Optional[str] = None
     work: Optional[str] = None
+    address: Optional[str] = None
 
 
-class PersonReq(BaseModel):
+@dataclass
+class PersonReq:
     name: str
     age: Optional[int] = None
-    address: Optional[str] = None
     work: Optional[str] = None
+    address: Optional[str] = None
 
 
 def get_person_by_id(person_id):
     with conn.cursor() as cur:
         cur.execute(
-            f"SELECT Id, Name, Age, Work, Address FROM persons WHERE Id = {person_id}"
+            f"SELECT \"Id\", \"Name\", \"Age\", \"Work\", \"Address\" FROM persons WHERE \"Id\" = \'{person_id}\'"
         )
         person_data = cur.fetchone()
-
         return PersonRes(*person_data) if person_data else None
 
 
 def get_persons_all():
     with conn.cursor() as cur:
         cur.execute(
-            "SELECT Id, Name, Age, Work, Address FROM persons"
+            "SELECT \"Id\", \"Name\", \"Age\", \"Work\", \"Address\" FROM persons"
         )
-        person_data = cur.fetchall()
+        persons_data = cur.fetchall()
 
-        return [PersonRes(*persons_list[i]) for i in len(person_data)] if person_data else None
+        return [PersonRes(*persons_data[i]) for i in range(len(persons_data))] if persons_data else None
 
 
 def person_insert(person: PersonReq):
     with conn.cursor() as cur:
         cur.execute(
-            f"INSERT INTO persons(Name, Age, Work, Address) \
-              VALUES ({person.name}, {person.age}, {person.work}, {person.address}) \
-              SELECT currval(pg_get_serial_sequence('persons','Id'))"
+            f"INSERT INTO persons(\"Id\", \"Name\", \"Age\", \"Work\", \"Address\") \
+              VALUES (DEFAULT, \'{person.name}\', {person.age}, \'{person.work}\', \'{person.address}\') \
+              RETURNING \"Id\";"
         )
         conn.commit()
         row = cur.fetchone()
-        return row
+        return row[0]
 
 
 def person_delete(person_id):
     with conn.cursor() as cur:
         cur.execute(
-            f"DELETE FROM persons WHERE Id = {person_id}"
+            f"DELETE FROM persons WHERE \"Id\" = \'{person_id}\'"
         )
         conn.commit()
 
@@ -68,43 +71,73 @@ def person_delete(person_id):
 def person_patch(person_id, person: PersonReq):
     with conn.cursor() as cur:
         cur.execute(
-            f"UPDATE person SET Name = {person.name}" +
-            f", Age = {person.age}" if person.age else "" +
-            f", Address = {person.address}" if person.address else "" +
-            f", Work = {person.work}" if person.work else "" +
-            f"WHERE Id = {person_id}"
+            f"UPDATE persons SET \"Name\" = \'{person.name}\'" +
+            (f", \"Age\" = \'{person.age}\'" if person.age else "") +
+            (f", \"Address\" = \'{person.address}\'" if person.address else "") +
+            (f", \"Work\" = \'{person.work}\'" if person.work else "") +
+            (f"WHERE \"Id\" = \'{person_id}\'")
         )
         conn.commit()
     return get_person_by_id(person_id)
 
 
-@app.get("/persons/{person_id}")
+@app.get("/api/v1/persons/{person_id}")
 def get_person(person_id: int):
     person = get_person_by_id(person_id)
     if person != None:
-        JSONResponse(content=person, status_code=status.HTTP_200_OK)
+        content = json.dumps(person,
+                             default=pydantic_encoder)
+        headers = {'Content-Type': 'application/json'}
+        return Response(content=content,
+                        headers=headers,
+                        status_code=status.HTTP_200_OK)
     else:
-        JSONResponse(status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content="Not found Person for ID",
+                            status_code=status.HTTP_404_NOT_FOUND)
 
 
-@app.get("/persons")
+@app.get("/api/v1/persons")
 def get_all_persons():
     persons = get_persons_all()
     if persons != None:
-        JSONResponse(content=persons, status_code=status.HTTP_200_OK)
+        content = json.dumps(persons, default=pydantic_encoder)
+        headers = {'Content-Type': 'application/json'}
+        return Response(content=content,
+                        headers=headers,
+                        status_code=status.HTTP_200_OK)
 
 
-@app.post("/persons")
+@app.post("/api/v1/persons")
 def post_person(person: PersonReq):
-    person_insert(person)
+    if person:
+        new_person_id = person_insert(person)
+        headers = {'Location': f'/api/v1/persons/{new_person_id}'}
+        return JSONResponse(content="Created new person",
+                            headers=headers,
+                            status_code=status.HTTP_201_CREATED)
+    else:
+        return JSONResponse(content="Invalid data",
+                            status_code=status.HTTP_400_BAD_REQUEST)
 
 
-@app.patch("/persons/{person_id}")
+@ app.patch("/api/v1/persons/{person_id}")
 def patch_person(person_id, person: PersonReq):
-    pass
+    person_by_id = get_person_by_id(person_id)
+    if person_by_id:
+        if person:
+            person = person_patch(person_id, person)
+            content = json.dumps(person, default=pydantic_encoder)
+            headers = {'Content-Type': 'application/json'}
+            return Response(content=content,
+                            headers=headers,
+                            status_code=status.HTTP_200_OK)
+        else:
+            return Response(status_code=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
 
 
-@app.delete("/persons/{person_id}")
+@ app.delete("/api/v1/persons/{person_id}")
 def delete_person(person_id: int):
     person_delete(person_id)
-    return JSONResponse(content="", status_code=status.HTTP_204_NO_CONTENT)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
